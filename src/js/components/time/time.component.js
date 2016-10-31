7d430b6f87e6df27a98e92d02b5a8e5f19129f94
Jelle Brouwer <jelle.brouwer@nr.ncia>
2016-10-31 16:43:54 +0100

    user cardinal color scheme for lines

--- src/js/components/time/time.component.js
+++ src/js/components/time/time.component.js
@@ -3,44 +3,60 @@
 
     angular
         .module('visualizations')
-        .component('time', {
-            template: '<svg id="line1" width="960" height="500"></svg>',
-            controller: TimeController,
-            bindings: {},
-        });
+        .directive('time', time);
 
-    function TimeController() {
+    function time() {
+        return {
+            restrict: 'E',
+            template: '<svg id="graph"></svg>',
+            link: link
+        };
+    }
+
+    function link(scope, element, attrs) {
         var $ctrl = this;
 
-        var graph = d3.select("#line1"),
-            margin = { top: 20, right: 20, bottom: 30, left: 50 },
-            width = +graph.attr("width") - margin.left - margin.right,
-            height = +graph.attr("height") - margin.top - margin.bottom,
-            g = graph.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        element.addClass('svg-container');
+
+        var margin = { top: 20, right: 120, bottom: 60, left: 60 },
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var graph = d3.select("#graph")
+            .attr("preserveAspectRatio", "xMinYMin meet")
+            .attr("viewBox", "0 0 960 500")
+            .attr('class', 'svg-content')
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         // set the ranges
-        var x = d3.scaleLinear().range([0, width]);
-        var y = d3.scaleLinear().range([height, 0]);
+        var x = d3.scaleLinear().range([0, width]),
+            y = d3.scaleLinear().range([height, 0]);
 
         // define the line
         var line = d3.line()
             .x(function (d) { return x(d.round); })
-            .y(function (d) { return y(d.score); });
+            .y(function (d) { return y(d.total); });
+
+        // Color scheme
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
 
         d3.json("mocks/score.json", function (error, response) {
             if (error) throw error;
 
-            var allScores = [];
-            var rounds = [];
+            var users = [], allScores = [], rounds = [], userNames = [];
+
             response.forEach(function (item, index) {
-                response[index] = response[index].scores.map(function (score, i) {
-                    return {
-                        round: i + 1,
-                        score: score.total
-                    }
-                });
+                users[index] = {
+                    scores: response[index].scores.map(function (score, i) {
+                        score.round = i + 1;
+                        return score;
+                    }),
+                    user: item.user
+                }
                 allScores = allScores.concat(item.scores);
                 rounds.push(item.scores.length);
+                userNames.push(item.user.name);
             });
 
             var mostRounds = rounds.reduce(function (a, b) {
@@ -60,26 +76,50 @@
                 return d.score;
             })]);
 
-            response.forEach(function (data) {
-                drawLine(data);
-            });
-
-            // Add the valueline path.
-            function drawLine(data) {
-                g.append("path")
-                    .data([data])
-                    .attr("class", "line")
-                    .attr("d", line);
-            }
             // Add the X Axis
-            g.append("g")
+            graph.append("g")
                 .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x))
-                .append("text");
+                .attr("class", "x-axis")
+                .call(d3.axisBottom(x).ticks(mostRounds));
 
             // Add the Y Axis
-            g.append("g")
+            graph.append("g")
+                .attr("class", 'y-axis')
                 .call(d3.axisLeft(y));
+
+            // Label X Axis
+            graph.append('text')
+                .attr("text-anchor", "middle")  // transform is applied to the anchor
+                .attr("transform",
+                "translate(" + (width / 2) + " ," +
+                (height + margin.top + 30) + ")")
+                .text("Round");
+
+            // text label for the y axis
+            graph.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 0 - margin.left)
+                .attr("x", 0 - (height / 2))
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text("Points");
+
+            var user = graph.selectAll(".user")
+                .data(users)
+                .enter().append("g")
+                .attr("class", "user");
+
+            user.append("path")
+                .attr("class", "line")
+                .attr("d", function (d) { return line(d.scores); })
+                .style("stroke", function (d) { return color(d.user.name); });
+
+            user.append("text")
+                .datum(function (d) { console.log(d.scores[d.scores.length - 1]); return { name: d.user.name, value: d.scores[d.scores.length - 1] }; })
+                .attr("transform", function (d) { return "translate(" + x(d.value.round) + "," + y(d.value.total) + ")"; })
+                .attr("x", 5)
+                .attr("dy", ".35em")
+                .text(function (d) { return d.name; });
         });
 
 
