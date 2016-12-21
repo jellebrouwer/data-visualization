938461790d8f47b2105890fcffc7f8d22586f674
Jelle Brouwer <jelle.brouwer@nr.ncia>
2016-12-21 16:54:08 +0100

    dom manipulation to link function

--- src/js/components/force/force.component.js
+++ src/js/components/force/force.component.js
@@ -17,24 +17,26 @@
         }
     }
 
-    function link(scope, element, attrs) {
-        var dimensions = element[0].getBoundingClientRect();
+    function link(scope, element, attrs, $ctrl) {
+
+        $ctrl.svg = d3.select(element[0])
+            .append('svg')
+            .attr("viewBox", "0 0 " + 960 + " " + 600)
+            .attr("preserveAspectRatio", "xMidYMid meet");
+
     }
 
     function ForceController() {
         var $ctrl = this;
-        var svg = d3.select("svg"), width, height, color, simulation;
+        var width, height, color, simulation;
 
         function init() {
-            svg = d3.select("svg");
-            width = +svg.attr("width");
-            height = +svg.attr("height");
             color = d3.scaleOrdinal(d3.schemeCategory20);
 
             simulation = d3.forceSimulation()
                 .force("link", d3.forceLink().id(function (d) { return d.id; }))
                 .force("charge", d3.forceManyBody())
-                .force("center", d3.forceCenter(width / 2, height / 2));
+                .force("center", d3.forceCenter(960 / 2, 600 / 2));
         }
 
         function getNetworkData() {
@@ -42,13 +44,15 @@
 
                 if (error) throw error;
 
-                var link = svg.append("g")
+                // Create links
+                var link = d3.select('svg').append("g")
                     .attr("class", "links")
                     .selectAll("line")
                     .data(graph.links)
                     .enter().append("line")
                     .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
 
+                // Create nodes
                 // var node = svg.append("g")
                 //     .attr("class", "nodes")
                 //     .selectAll("circle")
@@ -63,18 +67,19 @@
 
 
 
-                var label = svg.append("g")
+                var label = d3.select('svg').append("g")
                     .attr("class", "labels")
                     .selectAll("text")
                     .data(graph.nodes)
                     .enter().append("text")
                     .text(function (d) { return d.id });
 
-                var monster = svg.append('g')
+                var monster = d3.select('svg').append('g')
                     .attr('class', 'monsters')
                     .selectAll('path')
                     .data(graph.nodes)
                     .enter().append('path')
+                    .attr('class', 'monster')
                     .attr('d', 'M24 13.313c0-2.053-.754-3.026-1.417-3.489.391-1.524 1.03-5.146-.963-7.409-.938-1.065-2.464-1.54-4.12-1.274-1.301-.557-3.266-1.141-5.5-1.141s-4.199.584-5.5 1.141c-1.656-.266-3.182.208-4.12 1.274-1.993 2.263-1.353 5.885-.963 7.409-.663.463-1.417 1.435-1.417 3.489 0 .996.326 2.131.986 3.102-.485 1.421.523 3.049 2.283 2.854-.318 1.622 1.365 2.928 3.082 2.128-.201 1.163 1.421 2.58 3.443 1.569.671.572 1.188 1.034 2.204 1.034 1.155 0 1.846-.643 2.277-1.035 2.022 1.012 3.574-.406 3.374-1.569 1.718.8 3.4-.506 3.082-2.128 1.76.195 2.768-1.433 2.283-2.854.659-.97.986-2.106.986-3.101zm-12 6.57c-1.722 0-2.4-1.883-2.4-1.883h4.8s-.612 1.883-2.4 1.883zm3.578-2.992c-1.052-.515-2.455-1.126-3.578-.322-1.124-.804-2.526-.193-3.578.322-4.251 2.08-8.024-4.023-5.842-5.444.204-.132.488-.135.672-.053.661.292 1.406-.191 1.406-.914 0-2.214.692-4.434 2.154-5.988l.015-.01c2.604-2.596 7.741-2.596 10.345 0l.016.011c1.462 1.554 2.154 3.774 2.154 5.987 0 .726.748 1.205 1.406.914.141-.063.436-.1.671.053 2.15 1.392-1.514 7.561-5.841 5.444zm.172-7.391c-1.124 0-2.094.629-2.607 1.546-.373-.116-.744-.174-1.143-.174s-.77.058-1.143.174c-.513-.917-1.483-1.546-2.607-1.546-1.654 0-3 1.346-3 3s1.346 3 3 3c1.231 0 2.285-.748 2.747-1.811.246-.566.394-1.301 1.003-1.301s.758.735 1.003 1.301c.462 1.063 1.516 1.811 2.747 1.811 1.654 0 3-1.346 3-3s-1.346-3-3-3zm-7.5 4.5c-.828 0-1.5-.672-1.5-1.5s.672-1.5 1.5-1.5 1.5.672 1.5 1.5-.672 1.5-1.5 1.5zm7.5 0c-.828 0-1.5-.672-1.5-1.5s.672-1.5 1.5-1.5 1.5.672 1.5 1.5-.672 1.5-1.5 1.5z')
                     .call(d3.drag()
                         .on("start", dragstarted)
@@ -105,14 +110,14 @@
                         .attr("dy", function (d) { return d.y - 10; });
 
                     monster
-                        .attr("transform", function (d) { return 'translate(' + d.x + ',' + d.y + ')' })
+                        .attr("transform", function (d) { return 'translate(' + (d.x - 10) + ',' + (d.y - 10) + ')' })
                 }
             });
         }
 
         function setRadius() {
-            if (svg) {
-                svg.selectAll("circle")
+            if (d3.select('svg')) {
+                d3.select('svg').selectAll("circle")
                     .attr("r", $ctrl.config.radius);
             }
         }
